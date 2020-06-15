2504c3f9a63f1d3a2c02c8040bfbd6b3c59798bd
gcshift <gcshift@xinics.com>
2020-06-15 17:06:31 +0900

    getSeasonPlaytime 분리 및 함수 구현
    - 각 Season 별로 플레이타임을 가져오는 부분은 중복되어있음.
    URL_{SEASON} 형식으로 되어있던 기존 코드를 시즌 리스트로 분리하고 시즌 별 데이터를 가져오는 함수를 만듬.
    - Array를 사용하는 기존 코드를 명시적인 결과를 위하여 Object를 반환하도록 변경

--- lolchess.js
+++ lolchess.js
@@ -3,17 +3,8 @@ const cheerio = require("cheerio");
 
 const account = "디킨슨";
 
-const URL_A = "https://lolchess.gg/profile/kr"
-const URL_S3_5 = encodeURI(`${URL_A}/${account}/s3.5/matches`);
-const URL_S3 = encodeURI(`${URL_A}/${account}/s3/matches`);
-const URL_S2 = encodeURI(`${URL_A}/${account}/s2/matches`);
-
-const MATCH_URL_S3_5 = URL_S3_5 + '/all/'
-const MATCH_URL_S3 = URL_S3 + '/all/'
-const MATCH_URL_S2 = URL_S2 + '/all/'
-
-const url_list = [URL_S2, URL_S3, URL_S3_5];
-const match_url_list = [MATCH_URL_S2, MATCH_URL_S3, MATCH_URL_S3_5];
+const ROOT_URL = "https://lolchess.gg/profile/kr";
+const SEASONS = ['s2', 's3', 's3.5'];
 
 async function getHtml(url) {
     try {
@@ -23,7 +14,7 @@ async function getHtml(url) {
     }
 }
 
-function getMatchCount(html) {
+function getMatchCounts(html) {
     const $ = cheerio.load(html.data);
     const regex =  /\d+/;
     const matchCount = Math.ceil(parseInt($("h4").text().match(regex)) / 10);
@@ -40,7 +31,7 @@ function getMatchTime(html) {
     return parseInt(min) * 60 + parseInt(sec);
 }
 
-function secToTimeDelta(seconds) {
+function getTimespanWithSeconds(seconds) {
     var days = Math.floor(seconds / (60 * 60 * 24));
     seconds = seconds % (60 * 60 * 24);
     var hours = Math.floor(seconds / (60 * 60));
@@ -48,53 +39,31 @@ function secToTimeDelta(seconds) {
     var minutes = Math.floor(seconds / 60);
     seconds = seconds % 60
 
-    return [days, hours, minutes, seconds];
+    return { days, hours, minutes, seconds };
 }
 
-async function main() { 
-    const url_list = [URL_S2, URL_S3, URL_S3_5];
-    const [html_s2, html_s3, html_s3_5] = await Promise.all(url_list.map(url => getHtml(url)));
+const getSeasonPlaytime = async (season) => {
+    const url = encodeURI(`${ROOT_URL}/${account}/${season}/matches`);
+    const html = await getHtml(url);
+    const matchCounts = getMatchCounts(html);
+    const matchUrls = matchCounts.map(count => `${url}/all/${count}`);
+    const timeHtmls = await Promise.all(matchUrls.map(getHtml));
+    const time = timeHtmls.map(getMatchTime).reduce((x, y) => x + y, 0);
 
-    const matchCount_s2_list = getMatchCount(html_s2);
-    const matchCount_s3_list = getMatchCount(html_s3);
-    const matchCount_s3_5_list = getMatchCount(html_s3_5);
+    return {
+        season, time, timespan: getTimespanWithSeconds(time),
+    };
+};
 
-    var s2_url_list = [];
-    for(num in matchCount_s2_list) {
-        s2_url_list.push(MATCH_URL_S2 + num);
-    }
-
-    var s3_url_list = [];
-    for(num in matchCount_s3_list) {
-        s3_url_list.push(MATCH_URL_S3 + num);
-    }
-
-    var s3_5_url_list = [];
-    for(num in matchCount_s3_5_list) {
-        s3_5_url_list.push(MATCH_URL_S3_5 + num);
-    }
-
-    const s2_time_html_list = await Promise.all(s2_url_list.map(url => getHtml(url)));
-    const s3_time_html_list = await Promise.all(s3_url_list.map(url => getHtml(url)));
-    const s3_5_time_html_list = await Promise.all(s3_5_url_list.map(url => getHtml(url)));
-
-    var s2_time = 0;
-    for(var time_html of s2_time_html_list) {
-        s2_time += getMatchTime(time_html);
-    }
+async function getPlaytime() {
+    const seasons = await Promise.all(SEASONS.map(getSeasonPlaytime));
+    const total = seasons.reduce((x, y) => x + y.time, 0);
     
-    var s3_time = 0;
-    for(var time_html of s3_time_html_list) {
-        s3_time += getMatchTime(time_html);
-    }
-
-    var s3_5_time = 0;
-    for(var time_html of s3_5_time_html_list) {
-        s3_5_time += getMatchTime(time_html);
-    }
-
-    console.log([secToTimeDelta(s2_time) , secToTimeDelta(s3_time), secToTimeDelta(s3_5_time)]);
-    console.log(secToTimeDelta(s2_time + s3_time + s3_5_time))
+    return {
+        time: total,
+        timespan: getTimespanWithSeconds(total),
+        seasons,
+    };
 }
 
-main();
+getPlaytime().then(res => console.log(JSON.stringify(res, null, 4)));
